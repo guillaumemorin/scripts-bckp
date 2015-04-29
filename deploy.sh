301b8f26256575e6a526b073fc72bd55789b0217
Guillaume Morin <gmorin.pro@gmail.com>
2015-04-29 15:12:45 +0200

    refactoring backup method

--- deploy.sh
+++ deploy.sh
@@ -1,37 +1,48 @@
 #!/bin/sh
 
-update () {
-
-echo "$(tput setaf 1)Killing node...$(tput sgr 0)"
-pkill node 
-
+backup () {
 if [ -d "$demeteorized_path/node_modules/" ]; then
   echo "$(tput setaf 1)Backing up node modules...$(tput sgr 0)"
   mv "$demeteorized_path/node_modules/" "/tmp/node_modules_$timestamp"
 fi
+}
 
-demeteorizer
-
+restore () {
 if [ -d "/tmp/node_modules_$timestamp" ]; then
   mv "/tmp/node_modules_$timestamp" "$demeteorized_path/node_modules"
 fi
+}
 
+update () {
+
+stop 
+
+backup
+demeteorizer
+restore
 start
 echo "$(tput setaf 1)Restarted$(tput sgr 0)"
 
 }
 
+stop () {
+  echo "$(tput setaf 1)Killing node...$(tput sgr 0)"
+  pkill node
+}
+
 start () {
-  MONGO_URL=mongodb://localhost:27017/preprod ROOT_URL=http://localhost:3000 PORT=3000 node $demeteorized_path/main.js &
+  MONGO_URL=mongodb://localhost:27017/final_preprod2 PLATFORM=DEV ROOT_URL=http://localhost:3000 PORT=3000 node $demeteorized_path/main.js &
 }
 
 deploy () {
+
 if [ -d "$demeteorized_git" ]; then
   echo "$(tput setaf 1)Backing up .git directory...$(tput sgr 0)"
   mv "$demeteorized_git" "/tmp/demeteorized_git"
   restore_git=true
 fi
 
+backup
 demeteorizer
 cd $demeteorized_path
 
@@ -49,13 +60,16 @@ commit_msg="deploy "$(date)
 git commit -m "$commit_msg" 
 
 git push -f deploy master
+
+cd ..
+restore
+
 }
 
 
 demeteorized_path=".demeteorized"
 demeteorized_git="$demeteorized_path/.git"
 home_path=~
-nodegit_path="$home_path/npm_packages/nodegit"
 restore_git=false
 timestamp=$(date +%s)
 
